b55360bde45a9152a591b0f418ed19116de3787f
Giovanni Cappelletti <tech@methodwolf.com>
2024-06-19 10:06:51 +0200

    update

---
+++ src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+
+    // TODO
+    // definire variabili che caratterizzano un cerchio
+
+    // definire un costruttore
+
+    // definire metodo per calcolare il perimetro
+    // e area
+
+    // definire toString per stampare i valori
+}
+
+// FORMULA:
+// Perimetro = 2 * 3.14 * raggio
+// Area = 3.14 * raggio * raggio
